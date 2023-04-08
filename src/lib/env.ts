de6eb2b40b607f6c0576fa55c024a9306758ed58
Austin Milt <austin@holaplex.com>
2023-04-08 16:17:25 -0700

    use gcloud function url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,4 +4,5 @@ export const SPOTIFY_AUTH_REDIRECT_URL = "http://localhost:5173/callback";
 export const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
 export const SPOTIFY_ACCESS_TOKEN_URL =
   "https://accounts.spotify.com/api/token";
-export const SPOTME_TOP_ITEMS_URL = "http://localhost:8080";
+// export const SPOTME_TOP_ITEMS_URL = "http://localhost:8080";
+export const SPOTME_TOP_ITEMS_URL = "https://us-central1-wgmi-cc.cloudfunctions.net/spot-me-recommendations";
