03753bc745ecc37ca57f1cc7ac5660985afc8cbb
Austin Milt <austin@holaplex.com>
2023-04-08 16:26:19 -0700

    fix redirect

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,6 @@
 //TODO move to .env files
 export const SPOTIFY_CLIENT_ID = "238bfa72a67a4407825efc635e457c70";
-export const SPOTIFY_AUTH_REDIRECT_URL = "https://sage-duckanoo-bca3f3.netlify.app//callback";
+export const SPOTIFY_AUTH_REDIRECT_URL = "https://sage-duckanoo-bca3f3.netlify.app/callback";
 // export const SPOTIFY_AUTH_REDIRECT_URL = "http://localhost:5173/callback";
 export const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
 export const SPOTIFY_ACCESS_TOKEN_URL =
